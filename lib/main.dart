913970c4d48648405bdc4cb4f1a3df69afa8cf9e
june lee <melodysdreamj@gmail.com>
2024-02-05 14:31:48 +0900

    `

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-export 'src/util/di.dart';
-export 'src/util/ready/ready.dart';
+export 'util/di.dart';
+export 'util/ready/ready.dart';
 
 import 'package:flutter/material.dart';
 
-import 'src/util/start_app.dart';
+import 'util/start_app.dart';
 
 main() {
   return startApp();
